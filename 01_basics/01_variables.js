4e271e73d8eff11f4dffd7c93da1b8a4e67cfa09
KushGupta28 <guptakush.283203@gmail.com>
2026-03-22 19:28:52 +0000

    variables declaration

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "kush@google.com"
+var accountPassward = " 12345"
+accountCity = "jaipur"
+let accountState;
+console.log("accountId");
+accountEmail ="kg@kg,com"
+accountPassward ="5343"
+accountcity = "bengaluru"
+console.table([ accountEmail,accountId,accountPassward,accountcity, accountState])
