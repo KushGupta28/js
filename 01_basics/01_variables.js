dd2113aaec044f74caa4c20c5fe4a6717b422128
KushGupta28 <guptakush.283203@gmail.com>
2026-03-23 18:57:29 +0000

    kush

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,12 @@
 const accountId = 144553
 let accountEmail = "kush@google.com"
-var accountPassward = " 12345"
+var accountPassward = "12345"
 accountCity = "jaipur"
 let accountState;
-console.log("accountId");
-accountEmail ="kg@kg,com"
-accountPassward ="5343"
-accountcity = "bengaluru"
-console.table([ accountEmail,accountId,accountPassward,accountcity, accountState])
+
+accountEmail = "kg@kg,com"
+accountPassward = "5343"
+accountCity = "bengaluru"
+console.log(accountId)
+
+console.table([accountEmail, accountId, accountPassward, accountCity, accountState])   
