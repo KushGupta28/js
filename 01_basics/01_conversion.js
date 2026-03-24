590dd55657d7e57beb08ea00a7d05feab1cc8db6
KushGupta28 <guptakush.283203@gmail.com>
2026-03-24 19:14:12 +0000

    conversion

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,25 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+ //score =null;
+// console.log(typeof score);
+ //score=undefined;
+ //console.log(typeof score);
+ // 33=>"33"
+ //"33abc" =>NaN
+ // true =>1, false =>0
+ let isLoggedIn = 1;
+ let booleanisLoggedIn = Boolean(isLoggedIn);
+ console.log(typeof isLoggedIn);
+ console.log(typeof booleanisLoggedIn);
+ //1 =>true, 0 =>false
+ // ""= >false
+ //"kush" => true
+ let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(typeof someNumber);     // number
+console.log(typeof stringNumber);   // string
