1ba1e637ff2fbfa23e97995b56ced6b8b5d96cf9
KushGupta28 <guptakush.283203@gmail.com>
2026-03-24 18:11:29 +0000

    Datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,8 @@
+"use strict";// treat all js code as newer version
+let name = "kush"//string
+let age = 19 //numeric
+let isloggodIn = false // boolean
+ console.log (typeof"kush")
+ console.log (typeof age )
+console.log (typeof null)
+console.log (typeof undefine)
