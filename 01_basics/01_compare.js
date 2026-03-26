430bfde840d078502989f830fa990c52c5b8c6c1
KushGupta28 <guptakush.283203@gmail.com>
2026-03-26 11:34:34 +0000

    compaire

---
+++ 01_basics/01_compare.js
@@ -0,0 +1,16 @@
+console.log(2>5)
+console.log(2==6)
+console.log(4 <= 7)
+ // avoid these type of code
+ console.log("2" ==2)
+ console.log("03"==4)
+ console.log( null == 0)
+ console.log( null > 0)
+ console.log(null >= 0)
+ console.log( undefined ==0)
+ console.log( undefined <0)
+ console.log(undefined > 0)
+ // strict check "====" also check data type
+ console.log( "2" === 2)
+ 
+ 
